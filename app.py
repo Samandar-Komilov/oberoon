6b4f89e92fdce195cc013262bbb55d1abaacb83b
Samandar-Komilov <komilov.samandar07@gmail.com>
2024-07-23 02:34:50 +0500

    Basic routing

--- app.py
+++ app.py
@@ -1,16 +1,36 @@
 from webob import Request, Response
 
-from typing import Any
-
 
 class Oberoon:
+    def __init__(self):
+        self.routes = dict()
+
     def __call__(self, environ, start_response, *args, **kwargs):
         request = Request(environ)
         response = self.handle_request(request)
         return response(environ, start_response)
     
     def handle_request(self, request):
-        user_agent = request.environ.get("HTTP_USER_AGENT", "User Agent not found")
         response = Response()
-        response.text = "Hello World!"
+        handler = self.find_handler(request)
+        if handler:
+            handler(request, response)
+        else:
+            self.default_response(response)
+
         return response
+    
+    def find_handler(self, request):
+        for path, handler in self.routes.items():
+            if path == request.path:
+                return handler
+
+    def default_response(self, response):
+        response.status_code = 404
+        response.text = "Not found."
+
+    def route(self, path):
+        def wrapper(handler):
+            self.routes[path] = handler
+            return handler
+        return wrapper
--- main.py
+++ main.py
@@ -3,3 +3,20 @@ from app import Oberoon
 
 app = Oberoon()
 
+
+@app.route("/home")
+def home(request, response):
+    response.text = "Hello from the Home Page"
+
+
+@app.route("/about")
+def about(request, response):
+    response.text = "Hello from the About Page"
+
+
+"""
+{
+    "/home": home,
+    "/about": about
+}
+"""
