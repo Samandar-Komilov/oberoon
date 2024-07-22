e6df97a1fe13579feadd537f7fc7217f2392ee49
Samandar-Komilov <komilov.samandar07@gmail.com>
2024-07-22 12:41:21 +0500

    Very simple wsgi app using wsgiref (test)

---
+++ main.py
@@ -0,0 +1,13 @@
+from wsgiref.simple_server import make_server
+
+
+def sample_app(environ, start_response):
+    status = "200 OK"
+    headers = [("Content-type", "text/plain")]
+    start_response(status, headers)
+
+    return [b'Hello World']
+
+
+server = make_server("localhost", 8000, sample_app)
+server.serve_forever()
